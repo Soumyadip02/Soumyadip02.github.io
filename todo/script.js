75c7e836bcb850a7e7ad1d51ef52433ec36c543b
Soumyadip Bhattacharya <94463632+Soumyadip02@users.noreply.github.com>
2023-01-03 20:36:44 +0530

    Update script.js

--- todo/script.js
+++ todo/script.js
@@ -3,7 +3,7 @@ function Update() {
  title = document.getElementById("title").value;
  descp = document.getElementById("descp").value;
    if(title == '' && descp == ''){
-       alert("No data to add to list");
+       alert("No data to add to list\nPlease enter data to add😘");
      }
    else {
     if (localStorage.getItem("Items") == null) {
